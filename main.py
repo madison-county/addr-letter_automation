57f0af9086febcac8d781180637f65265313956b
boykjgis <boykjgis@gmail.com>
2023-06-07 11:27:45 -0600

    main.py: Create paragraph and edit text format

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ from docx.shared import Inches
 document = Document()
 
 document.add_heading('Madison County GIS Office', 0)
+
+p = document.add_paragraph('Lorem ipsum')
+p.add_run('bold').bold = True
+p.add_run(' and some ')
+p.add_run('italic').italic = True
