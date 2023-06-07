1dec765bbf149463c667e77b45c18602d8294e27
boykjgis <boykjgis@gmail.com>
2023-06-07 12:19:59 -0600

    main.py: Add main function and respective call

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 from docx import Document
 from docx.shared import Inches
 
-document = Document()
+def main():
+    document = Document()
 
-section = document.sections[0]
-header = section.header
+    section = document.sections[0]
+    header = section.header
 
-paragraph = header.paragraphs[0]
-paragraph.text = 'Madison County GIS Office'
+    paragraph = header.paragraphs[0]
+    paragraph.text = 'Madison County GIS Office'
 
-#document.add_heading('Madison County GIS Office', 0)
+    #document.add_heading('Madison County GIS Office', 0)
 
-p = document.add_paragraph('Lorem ipsum')
-p.add_run('bold').bold = True
-p.add_run(' and some ')
-p.add_run('italic').italic = True
+    p = document.add_paragraph('Lorem ipsum')
+    p.add_run('bold').bold = True
+    p.add_run(' and some ')
+    p.add_run('italic').italic = True
 
-#document.add_heading('Heading, level 1', level=0)
+    #document.add_heading('Heading, level 1', level=0)
 
-document.add_page_break()
+    document.add_page_break()
 
-document.save('demo.docx')
+    document.save('demo.docx')
+
+if __name__ == '__main__':
+    main()
